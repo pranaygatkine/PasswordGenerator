59c9af7b0f1bd3aecf999ab54b616f467f72f804
pranaygatkine <139122694+pranaygatkine@users.noreply.github.com>
2024-06-11 16:34:15 +0530

    Add files via upload

---
+++ PasswardGenerator/script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let btn = document.getElementById("generate");
+    btn.addEventListener("click", generatePassword);
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!-S^+";
+    const spaceChar = " ";
+
+    function getRandomChar(chars) {
+        const index = Math.floor(Math.random() * chars.length);
+        return chars[index];
+    }
+
+    function generatePassword() {
+        console.log("function called");
+        const passwordInput = document.getElementById("password");
+        const lowercaseCheckbox = document.getElementById("lowercase");
+        const uppercaseCheckbox = document.getElementById("uppercase");
+        const numbersCheckbox = document.getElementById("numbers");
+        const symbolsCheckbox = document.getElementById("symbols");
+        const excludeDuplicateCheckbox = document.getElementById("exc-duplicate");
+        const spacesCheckbox = document.getElementById("spaces");
+    
+        let characters = "";
+
+        if (lowercaseCheckbox.checked) characters += lowercaseChars;
+        if (uppercaseCheckbox.checked) characters += uppercaseChars;
+        if (numbersCheckbox.checked) characters += numberChars;
+        if (symbolsCheckbox.checked) characters += symbolChars;
+        if (spacesCheckbox.checked) characters += spaceChar;
+
+        if (characters === "") {
+            passwordInput.value = "";
+            return;
+        }
+
+        let password = "";
+        const length = 12; // Default password length
+
+        while (password.length < length) {
+            let char = getRandomChar(characters);
+
+            if (excludeDuplicateCheckbox.checked && password.includes(char)) continue;
+
+            password += char;
+        }
+
+        passwordInput.value = password;
+    }
+});
+
